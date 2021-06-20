f7794dde97624f0b37cf279fa098fab5856c451a
Patrick Garcia <patrickvicentegarcia@gmaill.com>
2021-06-20 17:52:23 -0300

    fix: update pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/global.scss'
 
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import { PlayerProvider } from '../contexts';
+
+import styles from '../styles/app.module.scss';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlayerProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+
+        <Player />
+      </div>
+    </PlayerProvider>
+  );
 }
 
 export default MyApp
